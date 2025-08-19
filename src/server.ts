1c4a8845a4eca9a9c7ebfb589e83a5ee00db9197
Guilherme Souza <gsouza2651@gmail.com>
2025-08-18 23:31:06 -0300

    feat: add initial Docker setup with Dockerfile, docker-compose, and .dockerignore

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.listen({ port: 8080 }, (error) => {
-	if (error) {
-		console.log(error);
-		process.exit(1);
-	}
+app.get('/', (_request, reply) => {
+  return reply.send({ data: 'hello world!' })
+})
 
-	console.log("Server listening on 8080");
-});
+app.listen({ port: 8080, host: '0.0.0.0' }, (error) => {
+  if (error) {
+    console.log(error)
+    process.exit(1)
+  }
+
+  console.log('Server listening on 8080')
+})
