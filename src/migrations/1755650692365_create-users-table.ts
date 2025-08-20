d523ef708b26eb14dde6a16e84c148b40373e926
Guilherme Souza <gsouza2651@gmail.com>
2025-08-19 21:52:19 -0300

    feat: add create-users-table migration
    - Added users table to the database

---
+++ src/migrations/1755650692365_create-users-table.ts
@@ -0,0 +1,24 @@
+import type { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate'
+
+export const shorthands: ColumnDefinitions | undefined = undefined
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  await pgm.createTable(
+    'users',
+    {
+      id: { type: 'UUID', primaryKey: true },
+      first_name: { type: 'VARCHAR(50)', notNull: true },
+      last_name: { type: 'VARCHAR(50)', notNull: true },
+      email: { type: 'VARCHAR(100)', notNull: true },
+      password_hash: { type: 'TEXT', notNull: true },
+      created_at: { type: 'TIMESTAMP', default: 'NOW()' },
+    },
+    {
+      ifNotExists: true,
+    }
+  )
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  await pgm.dropTable('users', { ifExists: true })
+}
