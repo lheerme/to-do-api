9c6555269d0bb2048ff8753017a35f5aae305be7
Guilherme Souza <gsouza2651@gmail.com>
2025-08-21 23:32:05 -0300

    test: add create-user use-case test.
    - Added vitest and @vitest/coverage-v8 as dependencies to make tests.
    - Added test, test:watch and test:coverage scripts to run tests.
    - Added `create-user-spec.ts` test for create-user use case.
    - Added `in-memory-users-repository.ts` to make tests.
    - Added coverage folder to .gitignore.

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,27 @@
+import type { User } from '../../interfaces/user.ts'
+import type { UsersRepository } from '../users-repository.ts'
+
+export interface InMemoryUsersRepositoryResponse extends UsersRepository {
+  users: User[]
+}
+
+export function InMemoryUsersRepository(): InMemoryUsersRepositoryResponse {
+  const users: User[] = []
+
+  async function createUser(
+    data: User
+  ): Promise<{ email: string; id: string }> {
+    await users.push(data)
+
+    const { email, id } = data
+
+    return { email, id }
+  }
+
+  async function isEmailInUse(email: string) {
+    const response = await users.find((user) => user.email === email)
+    return !!response
+  }
+
+  return { createUser, isEmailInUse, users }
+}
---
+++ src/use-cases/create-user.spec.ts
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import {
+  InMemoryUsersRepository,
+  type InMemoryUsersRepositoryResponse,
+} from '../repositories/in-memory/in-memory-users-repository.ts'
+import {
+  CreateUserUseCase,
+  type CreateUserUseCaseResponse,
+} from './create-user.ts'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.ts'
+
+let usersRepository: InMemoryUsersRepositoryResponse
+let createUser: CreateUserUseCaseResponse
+
+describe('Create user use case', () => {
+  beforeEach(() => {
+    usersRepository = InMemoryUsersRepository()
+    createUser = CreateUserUseCase(usersRepository)
+  })
+
+  it('should be abre do create users', async () => {
+    await createUser.execute({
+      firstName: 'Michael',
+      lastName: 'Scott',
+      email: 'ilovepaper@email.com',
+      password: 'ihatetoby',
+    })
+
+    const isEmailUsed = await usersRepository.isEmailInUse(
+      'ilovepaper@email.com'
+    )
+
+    expect(isEmailUsed).toBe(true)
+  })
+
+  it('should hash user password upon creation', async () => {
+    await createUser.execute({
+      firstName: 'Michael',
+      lastName: 'Scott',
+      email: 'ilovepaper@email.com',
+      password: 'ihatetoby',
+    })
+
+    const createdUserPasswordHash = usersRepository.users[0].password_hash
+    const isPasswordCorrectlyHashed = bcrypt.compareSync(
+      'ihatetoby',
+      createdUserPasswordHash
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to create user with same e-mail twice', async () => {
+    const email = 'ilovepaper@email.com'
+
+    await createUser.execute({
+      firstName: 'Michael',
+      lastName: 'Scott',
+      email,
+      password: 'ihatetoby',
+    })
+
+    await expect(async () => {
+      await createUser.execute({
+        firstName: 'Dwight',
+        lastName: 'Schrute',
+        email,
+        password: 'ilovemichael',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
