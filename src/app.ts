c82cd3ae91dfe064c3b3ed92ee5b7c652f6cfe2a
Guilherme Souza <gsouza2651@gmail.com>
2025-08-26 20:51:48 -0300

    feat: add logger to fastify app.
    - Added pino-pretty dependency.
    - Changed console.log to app.log inside app and server.

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,28 @@ import z, { ZodError } from 'zod'
 import { authenticateUser } from './controllers/authenticate-user.ts'
 import { createUser } from './controllers/create-user.ts'
 
-export const app = fastify()
+export const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+})
 
 app.post('/users', createUser)
 app.post('/sessions', authenticateUser)
 
-app.setErrorHandler((error, _request, reply) => {
+app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
     return reply
       .code(400)
       .send({ message: 'Validation error.', issues: z.flattenError(error) })
   }
 
-  // TODO: Utilizar o console.log apenas em desenvolvimento
-  console.log(error)
+  request.log.error(error)
   return reply.code(500).send({ message: 'Internal Error.' })
 })
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,7 @@ import { env } from './env.ts'
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }, (error) => {
   if (error) {
-    // TODO: Utilizar o console.log apenas em desenvolvimento
-    console.log(error)
+    app.log.error(error)
     process.exit(1)
   }
-
-  console.log(`Server listening on ${env.PORT}`)
 })
